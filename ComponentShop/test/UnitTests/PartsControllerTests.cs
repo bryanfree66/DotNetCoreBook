131d436f71fe660308cf0751dd830735e50c2001
Bryan Freeman <bryanfree66@gmail.com>
2017-06-18 21:08:56 -0700

    Added PartsController and tests

--- ComponentShop/test/UnitTests/PartsControllerTests.cs
+++ ComponentShop/test/UnitTests/PartsControllerTests.cs
@@ -1,14 +1,14 @@
-using System;
 using Xunit;
 
 namespace UnitTests
 {
-    public class UnitTest1
+    public class PartsControllerTests
     {
         [Fact]
         public void Test1()
         {
-
+            
         }
+        
     }
 }
