20b4fa0879021cef6558e074769e2aed341792ef
Bryan Freeman <bryanfree66@gmail.com>
2017-06-19 18:42:38 -0700

    Added component and component type models

---
+++ ComponentShop/src/Web/Models/Component.cs
@@ -0,0 +1,14 @@
+namespace Web.Models
+{
+    public class Component
+    {
+        public int Id{get;set;}
+        public string Name {get;set;}{
+        public string Code{get;set;}
+        public int Type{get;set;}
+        public string Description{get;set;}
+        public int Quantity{get;set;}
+        public decimal UnitPrice{get;set;}
+
+    }
+}
---
+++ ComponentShop/src/Web/Models/ComponentType.cs
@@ -0,0 +1,9 @@
+namespace Web.Models
+{
+    public class ComponentType
+    {
+        public int Id {get;set;}
+        public string TypeName{get;set;}
+        public string Description {get;set;}
+    }
+}
